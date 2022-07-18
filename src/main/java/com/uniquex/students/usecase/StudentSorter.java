ebb60c98d6f5ce283d947cbf89753038a93a3bbe
Algirdas <lapinskas.algirdas@gmail.com>
2022-07-18 15:28:23 +0800

    add time benchmark logging in StudentSorter

--- src/main/java/com/uniquex/students/usecase/StudentSorter.java
+++ src/main/java/com/uniquex/students/usecase/StudentSorter.java
@@ -2,13 +2,19 @@ package com.uniquex.students.usecase;
 
 import com.uniquex.students.domain.entity.port.Student;
 import com.uniquex.students.usecase.sort.algorithms.port.SortAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class StudentSorter {
+    private final Logger logger = LoggerFactory.getLogger(StudentSorter.class);
 
     public List<Student> sortStudentsBy(SortAlgorithm sortAlgorithm, List<Student> students) {
-        return sortAlgorithm.sort(students);
+        long start = System.nanoTime();
+        List<Student> sort = sortAlgorithm.sort(students);
+        logger.info(sortAlgorithm.getClass().getSimpleName() + " Sorted in: " +
+                (System.nanoTime() - start) + " nano");
+        return sort;
     }
 }
